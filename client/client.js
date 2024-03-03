3038a56cb8ff2648eb95f780dad704dd76f27852
Rited Das <rcd8710@g.rit.edu>
2024-03-02 20:06:02 -0500

    Fix line endings

--- client/client.js
+++ client/client.js
@@ -1,2 +1,46 @@
 console.log("client.js works so far");
-var socket = io();
+const socket = io();
+
+let roomID = null;
+
+function makeGame() {
+    socket.emit('makeGame');
+}
+
+function joinGame() {
+    roomID = document.getElementById('roomID').value;
+    socket.emit('joinGame', { roomID });
+}
+
+function getRoomLink() {
+    socket.emit('getRoomLink');
+}
+
+socket.on('roomCreated', (roomId) => {
+    console.log(`Room created with ID: ${roomId}`);
+    // You can now share the room ID with others
+    const roomUrl = window.location.origin + '/?roomId=' + roomId;
+    console.log('Room URL:', roomUrl);
+
+    // Automatically get and display the room link
+    getRoomLink();
+});
+socket.on('roomLink', (data) => {
+    const roomUrl = window.location.origin + '/?roomId=' + data.roomID;
+    console.log('Room Link:', roomUrl);
+    // Now you can share the room link with another player
+});
+
+socket.on("newGame", (data) => {
+    roomID = data.roomID;
+    document.getElementsByClassName("home-ui")[0].style.display = "none";
+    document.getElementsByClassName("Main-phase")[0].style.display = "block";
+});
+
+window.addEventListener('load', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const roomId = urlParams.get('roomId');
+    if (roomId) {
+        socket.emit('joinGame', roomId);
+    }
+});
---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/healthcheck', (req, res) => {
+    res.send('<h1>RPS App running...<h1>');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+// Store the rooms and their details
+const rooms = {};
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+io.on('connection', (socket) => {
+    console.log('a user has connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('makeGame', () => {
+        const roomID = makeid(6);
+        rooms[roomID] = {};
+        socket.join(roomID);
+        socket.emit('newGame', { roomID: roomID });
+    });
+
+    socket.on('joinGame', (data) => {
+        const roomId = data.roomID;
+        if (rooms[roomId] !== undefined) {
+            socket.join(roomId);
+            io.to(roomId).emit('2playersareconnected', {});
+            socket.emit('2playersconnected');
+        } else {
+            socket.emit('roomNotFound');
+        }
+    });
+
+    socket.on('getRoomLink', () => {
+        // Send the room ID back to the client
+        const roomID = Object.keys(rooms).find((roomId) => rooms[roomId] && Object.keys(rooms[roomId]).length < 2);
+        if (roomID) {
+            socket.emit('roomLink', { roomID });
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
