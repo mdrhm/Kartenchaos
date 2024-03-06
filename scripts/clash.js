42a216876c32fe8c0c007a904a2e38e9582aee63
Shakil Nabil <shakilnabil11@gmail.com>
2024-03-06 08:24:47 -0500

    minor changes to animation

--- scripts/clash.js
+++ scripts/clash.js
@@ -8,19 +8,28 @@ p1Hp.style.setProperty('--progress-color', 'rgba(14,50,6,1)');
 p2Hp.style.setProperty('--progress-color', 'rgba(14,50,6,1)');
 p1Attack.addEventListener('click', () => {
   p2Hp.value = attackEnemy(p2Hp.value, dmg);
+  if(p2Hp.value <= 0){
+    p2Hp.max = 100;
+    p2Hp.value = 100;
+  }
+  p2Hp.max = p2Hp.value;
+  updateWidth(p2Hp);
   updateBackgroundColor(p2Hp);
 });
 
 p2Attack.addEventListener('click', () => {
   p1Hp.value = attackEnemy(p1Hp.value, dmg);
+  if(p1Hp.value <= 0){
+    p1Hp.max = 100;
+    p1Hp.value = 100;
+  }
+  p1Hp.max = p1Hp.value;
+  updateWidth(p1Hp);
   updateBackgroundColor(p1Hp);
 });
 
 
 function attackEnemy(hp, dmg){
-  if(hp == 0){
-    return 100;
-  }
   hp -= dmg;
   return hp;
 }
@@ -44,5 +53,11 @@ function updateBackgroundColor(hp){
   else {
     hp.style.setProperty('--progress-color', 'rgba(134,37,16,100)');
   }
+
+}
+
+function updateWidth(hp) {
+  hp.style.width = `${hp.value}%`;
+  hp.style.transition = 'width 1s ease';
 }
 
