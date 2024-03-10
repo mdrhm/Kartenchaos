3559402c12f9aeb0cba3b9e74946d166881bd980
Shakil Nabil <shakilnabil11@gmail.com>
2024-03-10 16:53:41 -0400

    fixed health bars

--- scripts/clash.js
+++ scripts/clash.js
@@ -5,8 +5,8 @@ let p1Hp = document.getElementById('p1-health-bar');
 let p2Hp = document.getElementById('p2-health-bar');
 const dmg = 5;
 
-p1Hp.style.setProperty('--progress-color', 'rgba(30,100,10,1)');
-p2Hp.style.setProperty('--progress-color', 'rgba(30,100,10,1)');
+// p1Hp.style.setProperty('--progress-color', 'rgba(30,100,10,1)');
+// p2Hp.style.setProperty('--progress-color', 'rgba(30,100,10,1)');
 p1Attack.addEventListener('click', () => {
   p2Hp.value = attackEnemy(p2Hp.value, dmg);
   if(p2Hp.value <= 0){
@@ -38,30 +38,30 @@ function attackEnemy(hp, dmg){
 
 
 
-function updateBackgroundColor(hp){
+// function updateBackgroundColor(hp){
   
-  if (hp.value >= 85) {
-    hp.style.setProperty('--progress-color', 'rgba(30,100,10,1)');
-  } else if (hp.value >= 70) {
-    hp.style.setProperty('--progress-color', 'rgba(36,147,21,1)');
-  }
-  else if (hp.value >= 50) {
-    hp.style.setProperty('--progress-color', 'rgba(163,218,3,1)');
-  } 
-  else if (hp.value >= 25) {
-    hp.style.setProperty('--progress-color', 'rgba(210,185,41,1)');
-  }
-  else if (hp.value >= 15) {
-    hp.style.setProperty('--progress-color', 'rgba(190,39,6,1)');
-  }
-  else {
-    hp.style.setProperty('--progress-color', 'rgba(134,37,16,100)');
-  }
+//   if (hp.value >= 85) {
+//     hp.style.setProperty('--progress-color', 'rgba(30,100,10,1)');
+//   } else if (hp.value >= 70) {
+//     hp.style.setProperty('--progress-color', 'rgba(36,147,21,1)');
+//   }
+//   else if (hp.value >= 50) {
+//     hp.style.setProperty('--progress-color', 'rgba(163,218,3,1)');
+//   } 
+//   else if (hp.value >= 25) {
+//     hp.style.setProperty('--progress-color', 'rgba(210,185,41,1)');
+//   }
+//   else if (hp.value >= 15) {
+//     hp.style.setProperty('--progress-color', 'rgba(190,39,6,1)');
+//   }
+//   else {
+//     hp.style.setProperty('--progress-color', 'rgba(134,37,16,100)');
+//   }
 
-}
+// }
 
-function updateWidth(hp) {
-  hp.style.width = `${hp.value}%`;
-  hp.style.transition = 'width .5s ease';
-}
+// function updateWidth(hp) {
+//   hp.style.width = `${hp.value}%`;
+//   hp.style.transition = 'width .5s ease';
+// }
 
