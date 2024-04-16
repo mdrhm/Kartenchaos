1f8441d06793812207c6501e83330fcb97f9b4e6
mdrhm <mdtrahman04@gmail.com>
2024-04-16 18:14:34 -0400

    Custom Music

--- client/scripts/chat.js
+++ client/scripts/chat.js
@@ -15,7 +15,8 @@ document.querySelector(".chat-main-phase").addEventListener("click", () => {
 
 function sendChat(){
     if(chatText.value.replaceAll(" ", "").replaceAll("\n", "") !== ""){
-        sendMessage(chatText.value)
+        let player = (player1) ? "Player 1" : "Player 2"
+        socket.emit("sendMessage", {roomID: roomID, player: player, message: chatText.value})
     }
     setTimeout(() => {
         chatText.value = ""
@@ -30,3 +31,25 @@ chatText.addEventListener('keypress', function (e) {
         sendChat()
     }
 });
+
+socket.on("updateChat", (data) => {
+    switch(data.code) {
+        case "send":
+            let ampm = (new Date().getHours() < 12) ? "AM" : "PM"
+            let hour = (new Date().getHours() % 12 === 0) ? 12 : new Date().getHours() % 12
+            document.querySelector(".chat-inner").innerHTML += `<div class="message">
+                <div class="message-inner">
+                <h3 class="message-header">${data.player}
+                <span class="message-time">${hour + ":" + String(new Date().getMinutes()).padStart(2, '0') + " " + ampm}</span>
+                </h3>
+                    <p class="message-content">${data.message}<span class="message-edited hidden"> (edited)</span></p>
+                </div>
+                </div>`
+            document.querySelector(".chat-inner").scrollTo(0, document.querySelector(".chat-inner").scrollHeight)
+            if(document.querySelector(".chat").classList.contains("chat-close")){
+                document.querySelector(".chat-notification").classList.remove("hidden")
+                document.querySelector(".chat-notification").innerHTML = parseInt(document.querySelector(".chat-notification").innerHTML) + 1;
+            }
+            break;
+    }
+})
